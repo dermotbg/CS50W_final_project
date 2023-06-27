53ffbaada569d99104f6c96db290148814b68c83
Dermot Bateman <dermotbateman@gmail.com>
2023-06-27 14:50:59 +0300

    add replay audio function

--- BGpt/static/BGpt/audio.js
+++ BGpt/static/BGpt/audio.js
@@ -1,4 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
+    conversationLoop()
+})
+
+function playAudio(audio_B64) {
+    // create new audio context object
+    const audio_context = new AudioContext();
+
+    // create source to be played once decoded
+    const buffer_source = audio_context.createBufferSource();
+
+    // decode base 64 string
+    const audioData = atob(audio_B64)
+
+    // create array size of audio file
+    const audio_array_buffer = new Uint8Array(audioData.length)
+    
+    // store decoded data within array 
+    for(let i = 0; i < audioData.length; i++){
+        audio_array_buffer[i] = audioData.charCodeAt(i);
+    }
+    // decode the array into an audio buffer
+    audio_context.decodeAudioData(audio_array_buffer.buffer, buffer =>{
+        buffer_source.buffer = buffer;
+        // connect to output device and start playback
+        buffer_source.connect(audio_context.destination);
+        buffer_source.start(0);
+    });
+}
+
+function conversationLoop(){
     // def blob outside promise chain
     let blob;
     // set media reqs
@@ -38,8 +68,8 @@ document.addEventListener('DOMContentLoaded', () => {
             data = [];
 
             // below just for checking in browser
-            let url = window.URL.createObjectURL(blob);
-            rec_sound.src = url;
+            // let url = window.URL.createObjectURL(blob);
+            // rec_sound.src = url;
             
             const formData = new FormData();
             formData.append('audio', blob, 'audio.ogg')
@@ -52,6 +82,11 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(data => {
                 console.log(data);
                 playAudio(data.tts_resp);
+                replay = document.querySelector('#btn-replay')
+                replay.style.display = "block";
+                document.addEventListener('click', () => {
+                    playAudio(data.tts_resp);
+                })
             })
             .catch(error => console.log(error))
         }
@@ -59,31 +94,5 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .catch(function(error) {
         console.log(error.name, error.message);
-    });
-})
-
-function playAudio(audio_B64) {
-    // create new audio context object
-    const audio_context = new AudioContext();
-
-    // create source to be played once decoded
-    const buffer_source = audio_context.createBufferSource();
-
-    // decode base 64 string
-    const audioData = atob(audio_B64)
-
-    // create array size of audio file
-    const audio_array_buffer = new Uint8Array(audioData.length)
-    
-    // store decoded data within array 
-    for(let i = 0; i < audioData.length; i++){
-        audio_array_buffer[i] = audioData.charCodeAt(i);
-    }
-    // decode the array into an audio buffer
-    audio_context.decodeAudioData(audio_array_buffer.buffer, buffer =>{
-        buffer_source.buffer = buffer;
-        // connect to output device and start playback
-        buffer_source.connect(audio_context.destination);
-        buffer_source.start(0);
     });
 }
