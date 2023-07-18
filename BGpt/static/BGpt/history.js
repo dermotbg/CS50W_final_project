add02a2bbed005283850cd2ceec3ffb18c1af7c9
Dermot Bateman <dermotbateman@gmail.com>
2023-07-18 08:40:03 +0300

    add working history quicklinks

--- BGpt/static/BGpt/history.js
+++ BGpt/static/BGpt/history.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // pre-load chat if came from quick links 
+    const current_url = new URL(window.location.href);
+    const preLoad = current_url.hash.substring(1);
+    if (preLoad != null){
+        plChat = document.querySelectorAll(`.list-group-responses li[data-id="ch-${preLoad}"]`);
+            plChat.forEach(log => {
+                console.log(plChat)
+                log.style.display = 'block';
+                if (log.id.includes('resp')) {
+                  log.style['text-align'] = 'right';
+                };
+            })  
+    };
+
+
     const histItems = document.querySelectorAll('.list-group-item')
 
     histItems.forEach(item =>{
@@ -6,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault
 
             const chatSession = this.dataset.id;
-            console.log(chatSession);
+            // console.log(chatSession);
             // hide any open chats
             const allChats = document.querySelectorAll('.list-group-responses li');
             allChats.forEach(log => {
