b76d5716cd6e483e031dece2c50f1e0d969ef2b4
Dermot Bateman <dermotbateman@gmail.com>
2023-07-27 12:32:40 +0300

    implement full user data / pw change animation

--- BGpt/static/BGpt/profile.js
+++ BGpt/static/BGpt/profile.js
@@ -1,17 +1,62 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
-        // edit user info
+    // declare container variables
     editUser = document.querySelector('#edit-user');
-    editUser.addEventListener('click', function() {
-        container = document.querySelector('#change-user');
-        container.style.display = 'flex';
-        container.style.animationPlayState = "running"    
-    });
+    userContainer = document.querySelector('#change-user');
 
     editPw = document.querySelector('#edit-pw');
+    pwContainer = document.querySelector('#change-pw');
+
+    editUser.addEventListener('click', function() {
+        if (getComputedStyle(pwContainer).display === 'flex'){
+            pwContainer.style.animationPlayState = "paused";
+            pwContainer.classList.remove('slide-in');
+            pwContainer.classList.add('slide-out');
+            pwContainer.style.animationPlayState = "running";
+            if (userContainer.classList.contains('slide-out')){
+                userContainer.classList.remove('slide-out');
+            };
+            pwContainer.addEventListener('animationend', function() {
+                this.style.display = 'none';
+                userContainer.classList.add('slide-in');
+                userContainer.style.display = 'flex';
+                userContainer.style.animationPlayState = "running";   
+            }, {once:true});
+        }
+        else{
+            if (userContainer.classList.contains('slide-out')){
+                userContainer.classList.remove('slide-out');
+            };
+            userContainer.classList.add('slide-in');
+            userContainer.style.display = 'flex';
+            userContainer.style.animationPlayState = "running";   
+        }
+    });
+
+    
     editPw.addEventListener('click', function() {
-        container = document.querySelector('#change-pw');
-        container.style.display = 'flex';
-        container.style.animationPlayState = "running"    
+        if (getComputedStyle(userContainer).display === 'flex'){
+            userContainer.style.animationPlayState = "paused";
+            userContainer.classList.remove('slide-in');
+            userContainer.classList.add('slide-out');
+            userContainer.style.animationPlayState = "running";
+            if (pwContainer.classList.contains('slide-out')){
+                pwContainer.classList.remove('slide-out');
+            };
+            userContainer.addEventListener('animationend', function() {
+                this.style.display = 'none';
+                pwContainer.classList.add('slide-in');
+                pwContainer.style.display = 'flex';
+                pwContainer.style.animationPlayState = "running";
+            },{once:true});
+        }
+        else{
+            if (pwContainer.classList.contains('slide-out')){
+                pwContainer.classList.remove('slide-out');
+            };
+            pwContainer.classList.add('slide-in');
+            pwContainer.style.display = 'flex';
+            pwContainer.style.animationPlayState = "running";
+        }   
     });
 })
